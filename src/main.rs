f1f81a9e5b646fa9fd8c5b05d3c2844f50e305e9
Ni Sel <nselyavin@inbox.ru>
2023-01-12 10:30:29 +0300

    inited map module

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 
 mod map;
 
-fn main() {
-    println!("Hello, world!");
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
 }
+
+use prelude::*;
+
+fn main() {
+    
+    println!("Hello, world!");
+} 
